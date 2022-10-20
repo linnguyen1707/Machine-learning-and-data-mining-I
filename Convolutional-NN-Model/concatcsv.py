8abc7c755897d9f1a17a339a5ee022eec6c0d5cd
Ph1eu <hieupm.bi11-090@st.usth.edu.vn>
2022-10-20 13:56:16 +0700

    concat history #2

--- Convolutional-NN-Model/concatcsv.py
+++ Convolutional-NN-Model/concatcsv.py
@@ -3,14 +3,15 @@ import glob
 import os
 
 
-first_100_epoch = pd.read_csv("./cnn_with_dropout_batchnorm_first100ep/history/cnn3model.csv")
+first_500_epoch = pd.read_csv("../history-report/CNN-history.csv")
 #next_200_epoch = pd.read_csv("./cnn_with_dropout_batchnorm_300ep/history/cnn3model.csv")
-last_200_epoch = pd.read_csv("./cnn_with_dropout_batchnorm_500ep/history/cnn3model.csv")
-file = [first_100_epoch,last_200_epoch]
+next_750_epoch = pd.read_csv("./cnn_with_dropout_batchnorm_750ep/history/cnn3model.csv")
+
+file = [first_500_epoch,next_750_epoch]
 #print(first_100_epoch)
 history = pd.concat(file,ignore_index=True)
+history['epoch']=[x for x in range(0,750)]
 print(history.index)
-#history.to_csv("./test1.csv")
-history['epoch']=[x for x in range(0,500)]
+#history.to_csv("./test1.csv",index=False)
 print(history['epoch'])
-history.to_csv("../history-report/CNN-history.csv",index=False)
+history.to_csv("../history-report/CNN-history-v2.csv",index=False)
