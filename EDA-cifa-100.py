47899061ead3fa0b425ec9d57e4bed1ecddf40e0
linnguyen1707 <91494583+linnguyen1707@users.noreply.github.com>
2022-10-26 11:58:33 +0700

    Update and rename EDA-cifa-100.ipynb to EDA-cifa-100.py

--- EDA-cifa-100.py
+++ EDA-cifa-100.py
@@ -63,6 +63,8 @@ for i in np.arange(0, W_grid * L_grid): # create evenly spaces variables
     axes[i].set_title(labels[label_index], fontsize = 20)
     axes[i].axis('off')
 
+# In this notebook we are going to build a CNN model that can classify images of various objects. We have many class of images:apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm'
+# show the plot how many image each type                                                 
 plt.subplots_adjust(hspace=0.4)
 
 classes_name = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 
@@ -71,3 +73,39 @@ classes_name = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee'
 classes, counts = np.unique(y_train, return_counts=True)
 plt.barh(classes_name, counts)
 plt.title('Class distribution in training set')
+
+#The readme located at cs.toronto.edu states that there is an even distribution of all classes, There are 500 training images and 100 testing images per class. The 100 classes in the CIFAR-100 are grouped into 20 superclasses
+def show_unique_images(images, labels, class_names=class_names_list):
+    unique_labels = []
+    unique_indices = []
+    
+    fig = plt.figure(figsize=(25,10))
+    
+    n = 0
+    for i in range(len(labels)):
+        if labels[i] not in unique_labels:
+            image = images[i].reshape(3,32,32).transpose(1,2,0)
+            plt.subplot(2,5,n+1)
+            n += 1
+            plt.imshow(image, interpolation="nearest")
+            plt.title(class_names[labels[i]])
+            unique_labels.append(labels[i])
+            unique_indices.append(i)
+    plt.show()
+    
+    return unique_indices
+                                                  
+    
+                                                  
+    def show_images(images, labels, indices, class_names=class_names_list):
+    fig = plt.figure(figsize=(25,10))
+    n = 0
+    for i in indices:
+        image = images[i].reshape(3,32,32).transpose(1,2,0)
+        plt.subplot(2,5,n+1)
+        n += 1
+        plt.imshow(image)
+        plt.title(class_names[labels[i]])
+    plt.show()
+    
+    return None
